e4ea4af93c89f9504fdad792e8369e97a4532c70
Md Tariqul Islam <topu.hit@gmail.com>
2022-12-20 22:17:56 +0600

    avif added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,8 @@
 	import Image4 from '$lib/assets/1.jpg?w=400&h=300&webp'
 	import Image5 from '$lib/assets/1.jpg'
 
+	import helloA from '$lib/assets/1.jpg?w=400&h=300&avif'
+	import helloAH from '$lib/assets/1.jpg?w=800&h=600&avif'
 	import hello1 from '$lib/assets/1.jpg?w=400&h=300&webp'
 	import hello2 from '$lib/assets/1.jpg?w=800&h=600&webp'
 	import hello3 from '$lib/assets/1.jpg?w=400&h=300&jpg'
@@ -24,14 +26,15 @@
 
 <picture>
 	<source srcset="{Image2}" type="image/avif">
-		<source srcset="{Image}" type="image/webp"> 
-			<img src="{Image3}" alt="sdfsdf">
+	<source srcset="{Image}" type="image/webp"> 
+		<img src="{Image3}" alt="sdfsdf">
 	</picture>
 
 
 
 <br>
 	<picture>
+		<source srcset="{helloA} 1x,{helloAH} 2x" type="image/avif">
 		<source srcset="{hello1} 1x,{hello2} 2x" type="image/webp">
 			
 		<img src="{hello3}" srcset="{hello3} 1x,{hello4} 2x">
