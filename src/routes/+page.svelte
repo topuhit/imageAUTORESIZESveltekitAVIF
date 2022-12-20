196ac635407a8d00f36b2d9c4dd0435eb4e3815b
Md Tariqul Islam <topu.hit@gmail.com>
2022-12-20 15:04:25 +0600

    image example for 1x 2x with webp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,11 @@
 	import Image4 from '$lib/assets/1.jpg?w=400&h=300&webp'
 	import Image5 from '$lib/assets/1.jpg'
 
+	import hello1 from '$lib/assets/1.jpg?w=400&h=300&webp'
+	import hello2 from '$lib/assets/1.jpg?w=800&h=600&webp'
+	import hello3 from '$lib/assets/1.jpg?w=400&h=300&jpg'
+	import hello4 from '$lib/assets/1.jpg?w=800&h=600&jpg'
+
 
 </script>
 
@@ -21,4 +26,13 @@
 	<source srcset="{Image2}" type="image/avif">
 		<source srcset="{Image}" type="image/webp"> 
 			<img src="{Image3}" alt="sdfsdf">
+	</picture>
+
+
+
+<br>
+	<picture>
+		<source srcset="{hello1} 1x,{hello2} 2x" type="image/webp">
+			
+		<img src="{hello3}" srcset="{hello3} 1x,{hello4} 2x">
 	</picture>
