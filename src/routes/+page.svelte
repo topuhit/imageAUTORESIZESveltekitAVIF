6a12121b338ebe91f92bc640c782e29efe2b9f19
Md Tariqul Islam <topu.hit@gmail.com>
2022-12-22 20:57:42 +0600

    image transformation combined in array

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
 	import Image3 from '$lib/assets/1.jpg?w=400&h=300&jpg'
 	import Image4 from '$lib/assets/1.jpg?w=400&h=300&webp'
 	import Image5 from '$lib/assets/1.jpg'
+	import ImageCom from '$lib/assets/1.jpg?width=400;800&format=avif;webp;jpg'
 
 	import helloA from '$lib/assets/1.jpg?w=400&h=300&avif'
 	import helloAH from '$lib/assets/1.jpg?w=800&h=600&avif'
@@ -14,11 +15,21 @@
 	import hello3 from '$lib/assets/1.jpg?w=400&h=300&jpg'
 	import hello4 from '$lib/assets/1.jpg?w=800&h=600&jpg'
 
+// console.table(ImageCom)
 
 </script>
 
 
 <h1>Vite Imagetool auto resize and avif, webp auto format images.</h1>
+
+
+	<picture>
+		<source srcset="{ImageCom[0]} 1x,{ImageCom[3]} 2x" type="image/avif">
+		<source srcset="{ImageCom[1]} 1x,{ImageCom[4]} 2x" type="image/webp">
+			
+		<img src="{ImageCom[2]}" srcset="{ImageCom[2]} 1x,{ImageCom[5]} 2x">
+	</picture>
+
 
 
 	<picture>
